067b30a503574b6288c87e7acfc960c0ae2440f1
eferreyro <emafe7472@gmail.com>
2019-09-06 12:27:42 -0500

    Mejorando la vista Registro con VUE

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+<b-container>
+        <b-row align-h="center">
+            <b-col cols="8">
+                <b-card-group deck>
+                    <b-card header="Registro"
+                            footer="By eferreyro"
+                            title="ALTA DE USUARIOS"
+                            class="my-3"
+                    >
+                <b-form  method="POST" action="{{ route('register') }}">
+                            {{ csrf_field() }}
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+            <b-form-group
+                    label="Nombre completo" label-for="name"
+                    description="Ingresa tu nombre completo">
+                    <b-form-input type="text"
+                        id="name" name="name"
+                        value="{{ old('name') }}" required autofocus
+                        placeholder="Nombre y Apellido">
+                    </b-form-input>
+            </b-form-group>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+            <b-form-group
+                label="Email" label-for="email"
+                description="Ingresa tu email. Te enviaremos una clave de cofirmacion">
+                <b-form-input type="email"
+                    id="email" name="email"
+                    value="{{ old('email') }}" required 
+                    placeholder="email@ejemplo.com">
+                </b-form-input>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+            </b-form-group>
+            
+            <b-form-group
+                label="Contraseña" label-for="password"
+                description="la contraseña debe ser al menos de 8 caracteres"
+                >
+                <b-form-input type="password"
+                    id="password" name="password"
+                    required 
+                    placeholder="********">
+                </b-form-input>
+            </b-form-group>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+            <b-form-group
+                label="Confirmar ontraseña" label-for="password_confirmation"
+                >
+                <b-form-input type="password"
+                    id="password_confirmation" name="password_confirmation"
+                    required 
+                    placeholder="********">
+            </b-form-input>
+        </b-form-group>
+            
+            </b-col>
+        </b-row>
+</b-container>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  
 @endsection
